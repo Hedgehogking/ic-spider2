f24c68c64cf356d027754009c4c4c03bbe11f525
hedgehog <imhedgehog@qq.com>
2021-08-14 15:44:39 +0800

    perf: 破解反爬虫的图片验证码逻辑,整理循环获取数据的逻辑

--- src/search.js
+++ src/search.js
@@ -6,107 +6,112 @@ const MAX_RT = 3;
 
 async function getBrowser() {
 	let tempbrowser;
-	for (let i = MAX_RT; i > 0; i--) {
+	let i = MAX_RT;
 
-		if (tempbrowser) {
-			break;
-		}
-
-		console.log('start to init browser...');
-		tempbrowser = await puppeteer.launch({
-		headless:false,
-    ignoreDefaultArgs: ["--enable-automation"],
-		headers: {
-			'User-Agent':'Baiduspider',
-		},
-		args: [
-			'--window-size="1200,1000"',
-		//   '--start-fullscreen',
-		//   '--proxy-server=socks5://127.0.0.1:1080'
-		]
-		}).catch(ex => {
-			if (i-1 > 0) {
-				console.log('browser launch failed. now retry...');
-			} else {
-				console.log('browser launch failed!');
-			}
-		});
+	function luanch() {
+		return puppeteer.launch({
+			headless:false,
+			ignoreDefaultArgs: ["--enable-automation"],
+			headers: {
+				'User-Agent':'Baiduspider',
+			},
+			args: [
+				'--window-size="1200,1000"',
+			//   '--start-fullscreen',
+			//   '--proxy-server=socks5://127.0.0.1:1080'
+			]
+			}).catch(async ex => {
+				if (--i > 0) {
+					console.log('browser launch failed. now retry...');
+					const browser = await luanch();
+					return browser;
+				} else {
+					console.log('browser launch failed!');
+				}
+				return null;
+			});
 	}
-
+	console.log('start to init browser...');
+	tempbrowser = await luanch();
 	if (!tempbrowser) {
 		console.log('fail to launch browser');
 		return null;
 	}
 	return tempbrowser;
 }
-async function loadWebsite(page, url) {
-	let respond;
-	for (let i = MAX_RT; i > 0; i--) {
-		if (respond) {
-			break;
-		}
-		console.log('start to expose function "window.stop()"');
-		console.log('start to goto page...');
-		respond = await page.goto(url, {
+
+async function loadWebsite(page, url, model) {
+	let i = MAX_RT;
+	function load() {
+		console.log(`${model} load url: ${url}`);
+		return page.goto(url, {
 			'waitUntil':'domcontentloaded',
-			'timeout':6000000
-		}).catch(ex=>{
-			if(i-1 > 0) {
+			'timeout':60000
+		}).catch(async ex=>{
+			if(--i > 0) {
 				console.log('fail to goto website. now retry...');
+				await load();
 			} else {
 				console.log('fail to goto website!');
 			}
 		});
 	}
-	return respond;
+	console.log('start to goto page...');
+	await load();
 }
 
 async function getPageType(page) {
-	const { type } = await page.evaluate(() => {
-    const data = {
-      type: 0,
-    }
-		const searchCode = document.querySelector('#searchCode');
-			if (searchCode) {
-				// 验证码页
-				data.type = 1;
-			}
-		return data;
-	});
-	return type;
+  await page.waitForSelector('.footer');
+	let tmpType = 0;
+	let resultList = await page.$('#resultList');
+	let return_empty = await page.$('.return_empty');
+	let searchCode = await page.$('#searchCode');
+	console.log(!!resultList, !!return_empty, !!searchCode);
+	if (resultList) {
+		// 搜索页
+		tmpType = 0
+	} else if (return_empty) {
+		// 空搜索页
+		tmpType = 1;
+	} else if (searchCode) {
+		// 验证码页
+		tmpType = 2;
+	} else {
+		// 其他页
+		tmpType = 3;
+	}
+	return tmpType;
 }
 
-module.exports = class Search {
-	constructor(models) {
-		this.models = models;
-		this.init();
+class Page {
+	constructor(browser, page, model, pageNum, CHAPTERS_URL) {
+		this.browser = browser;
+		this.page = page;
+		this.model = model;
+		this.pageNum = pageNum;
+		this.CHAPTERS_URL = CHAPTERS_URL;
+
+		this.run(page, model, pageNum, CHAPTERS_URL);
 	}
 
-	async init() {
-		this.browser = await getBrowser();
-		this.models.forEach(async model => {
-			const page = await this.browser.newPage().catch(err=>{
-				console.log(err);
-			});
-			if (!page) {
-				console.log('fail to open page!');
-				return;
-			}
-			let pageNum = 1;
-			// let CHAPTERS_URL = `https://www.ic.net.cn/searchPnCode.php`;
-			let CHAPTERS_URL = `https://www.ic.net.cn/search/${model}.html`;
-			// 不行就用代理
-			await page.exposeFunction('stop', () => { return Promise.resolve('already stop') })
-			this.run(page, model, pageNum, CHAPTERS_URL);
-		})
-	}
-
-	async run(page, model, pageNum, CHAPTERS_URL) {
-		await loadWebsite(page, `${CHAPTERS_URL}?page=${pageNum}`);
-		// const type = await getPageType(page);
-		// if (type === 1) {
-		// 	await hackVerificationCode(page);
-		// }
+	async run(page, model, pageNum, CHAPTERS_URL){
+		this.history = { CHAPTERS_URL, pageNum };
+		await loadWebsite(page, `${CHAPTERS_URL}?page=${pageNum}`, model);
+		const type = await getPageType(page);
+		if (type === 1) {
+			// 空搜索页
+			// return;
+		} else if (type === 2) {
+			// 验证码页
+			await hackVerificationCode(page);
+			// 验证码完成
+			// TODO：
+			return;
+		} else if (type === 3) {
+			// 其他页
+			await this.run(page, model, this.history.pageNum, this.history.CHAPTERS_URL);
+			return;
+		}
 		const { chapterTitle, chapterList } = await getSearchResult(page);
 		if (chapterList.length) {
 			console.log('\x1B[32m%s\x1B[0m', `
@@ -133,80 +138,30 @@ module.exports = class Search {
 		});
 	}
 }
-/*
-module.exports = async function search(model) {
-	const CHAPTERS_URL = `https://www.ic.net.cn/search/${model}.html?page=${pageNum}`;		//目录地址
 
-	var browser = tempbrowser;
-
-	console.log('start to new page...');
-	var page = await browser.newPage().catch(ex=>{
-		console.log(ex);
-	});
-	if (!page) {
-		await browser.close().catch();
-		reject('fail to open page!');
-		return;
+module.exports = class Search {
+	constructor(models) {
+		this.models = models;
+		this.init();
 	}
 
-	page.on("load", async () => {
-		console.log(`load page success => ${page.url()}`);
-
-		if (page.url() === CHAPTERS_URL) {
-
-			// puppeteer 方式
-			console.log('load the page');
-			const { chapterTitle, chapterList } = await getSearchResult(browser, page);
-			console.log('list length: ', chapterList.length);
-			// TODO: 翻页
-
-
-			if (chapterList.length) {
-				console.log('get page data success !!!!!!!!!!');
-				// const excel = new Excel(chapterTitle, chapterList, companyName);
-				// await excel.writeExcel();
-				console.log('\x1B[32m%s\x1B[0m', `${model} search success`);
-				console.log('close the browser');
-			} else {
-				console.log('get page data empty');
-			}
-
-			await browser.close().catch(ex=>{
-				console.log('fail to close the browser!');
+	async init() {
+		this.browser = await getBrowser();
+		this.models.forEach(async model => {
+			const page = await this.browser.newPage().catch(err=>{
+				console.log(err);
 			});
-
-		}
-	})
-
-	var respond;
-	for (var i = MAX_RT; i > 0; i--) {
-
-		if (respond) {
-			break;
-		}
-    console.log('start to expose function "window.stop()"');
-    // 不行就用代理
-    await page.exposeFunction('stop', () => { return Promise.resolve('already stop') })
-		console.log('start to goto page...');
-		respond = await page.goto(CHAPTERS_URL, {
-			'waitUntil':'domcontentloaded',
-			'timeout':120000
-		}).catch(ex=>{
-			if(i-1 > 0) {
-				console.log('fail to goto website. now retry...');
-			} else {
-				console.log('fail to goto website!');
+			if (!page) {
+				console.log('fail to open page!');
+				return;
 			}
-
-		});
-	}
-  await page.evaluate((num) => {
-  }, 1).catch(() => {
-  })
-	if (!respond) {
-		await browser.close().catch();
-		reject('fail to go to website!');
-		return;
+			let pageNum = 1;
+			// let CHAPTERS_URL = `https://www.ic.net.cn/searchPnCode.php`;
+			let CHAPTERS_URL = `https://www.ic.net.cn/search/${model}.html`;
+			// 不行就用代理
+			console.log('start to expose function "window.stop()"');
+			await page.exposeFunction('stop', () => { return Promise.resolve('already stop') })
+			new Page(this.browser, page, model, pageNum, CHAPTERS_URL);
+		})
 	}
 }
- */
