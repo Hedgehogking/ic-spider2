73cd7f0ea3f33f9e763f244c69562775e64dbe22
hedgehog <imhedgehog@qq.com>
2021-08-14 15:46:14 +0800

    feat: 整理爬虫逻辑

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const hackImg = require('./src/hack-img')
 const Search = require('./src/search')
-const fs = require('fs');
-const path = require('path');
 
-new Search(['ADXL1002BCPZ', 'ATMEGA32A-PU'])
+new Search([/* 'ADXL1002BCPZ', 'ATMEGA32A-PU', 'STM32F103C8T6', */ 'TPS51200DRCR', 'STM32F407VGT6'])
 
-function all() {
-  const dir = path.join(__dirname, './hack-images/');
-  const list = fs.readdirSync(dir)
-  list.slice(0, 2).forEach(async fileName => {
-    if (fileName.match(/\.jpg$/)) {
-      await hackImg(`${dir}${fileName}`)
-    }
-  });
-}
-
-// all()
-// hackImg('/Volumes/Work/project/ic-spider2/code_000000.jpg')
-// hackImg('/Users/Hedgehog/Downloads/training-code/code_832390.jpg')
