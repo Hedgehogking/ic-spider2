0d0bc61ec3a0a6578128e84ba2517fe09524d46e
hedgehog <imhedgehog@qq.com>
2021-08-14 15:41:20 +0800

    perf: 循环提交验证码

--- src/hack-verification-code.js
+++ src/hack-verification-code.js
@@ -3,11 +3,40 @@ const fs = require('fs');
 const path = require('path');
 
 const HACK_CODE_IMAGES_PATH = path.join(__dirname, '../hack-images');
+const VERIFICATION_CODE = '#searchCode';
 
 function createDir(path) {
   if (!fs.existsSync(path)) {
     fs.mkdirSync(path);
   }
+}
+
+async function findAndhackCode(page) {
+  await page.waitForSelector(VERIFICATION_CODE);
+  await page.evaluate(async (HACK_CODE_IMAGES_PATH, VERIFICATION_CODE) => {
+    const codeFileName = `code_${Math.floor(Math.random() * 1000000)}.jpg`;
+
+    const searchCode = document.querySelector(VERIFICATION_CODE);
+    if (!searchCode) {
+      console.log('end, no searchCode');
+      return;
+    }
+    searchCode.width = 120;
+    searchCode.height = 50;
+    // 创建验证码存放处
+    createDir(HACK_CODE_IMAGES_PATH)
+    await screenshot({
+      clip: searchCode.getBoundingClientRect(),
+      path: `${HACK_CODE_IMAGES_PATH}/${codeFileName}`,
+      quality: 100,
+    })
+    const code = await hackImg(`${HACK_CODE_IMAGES_PATH}/${codeFileName}`);
+    searchCode.previousElementSibling.value = code;
+    return code;
+  }, HACK_CODE_IMAGES_PATH, VERIFICATION_CODE).catch(err => {
+    console.log(err);
+  });
+  await page.click('[name="btn_submit"]');
 }
 
 module.exports = async function hackVerificationCode(page) {
@@ -18,23 +47,13 @@ module.exports = async function hackVerificationCode(page) {
   await page.exposeFunction('createDir', createDir);
   await page.exposeFunction('screenshot', screenshot);
   await page.exposeFunction('hackImg', hackImg);
-  await page.evaluate(async (HACK_CODE_IMAGES_PATH) => {
-    const VERIFICATION_CODE = '#searchCode';
-    const codeFileName = `code_${Math.floor(Math.random() * 1000000)}.jpg`;
-
-    const searchCode = document.querySelector(VERIFICATION_CODE);
-    searchCode.width = searchCode.width * 5;
-    searchCode.height = searchCode.height * 5;
-    // 创建验证码存放处
-    createDir(HACK_CODE_IMAGES_PATH)
-    await screenshot({
-      clip: searchCode.getBoundingClientRect(),
-      path: `${HACK_CODE_IMAGES_PATH}/${codeFileName}`,
-      quality: 100,
-    })
-    const code = await hackImg(`${HACK_CODE_IMAGES_PATH}/${codeFileName}`);
-    // console.log(code);
-  }, HACK_CODE_IMAGES_PATH).catch(err => {
-    console.log(err);
-  });
+  page.on('dialog', async (dialog) => {
+    console.log(dialog.message());
+    await dialog.accept();
+    // 再刷
+    setTimeout(async () => {
+      await findAndhackCode(page);
+    }, 0);
+  })
+  await findAndhackCode(page);
 }
