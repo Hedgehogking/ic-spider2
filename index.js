eb29ded412647f41381ce3c5d82a24ff8e491f11
hedgehog <imhedgehog@qq.com>
2021-08-25 13:02:32 +0800

    test: 调整搜索的型号

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const Search = require('./src/search')
 
-new Search([/* 'ADXL1002BCPZ', 'ATMEGA32A-PU', 'STM32F103C8T6', */ 'TPS51200DRCR', 'STM32F407VGT6'])
-
+new Search(['ADXL1002BCPZ', 'ATMEGA32A-PU'/* , 'STM32F103C8T6', 'TPS51200DRCR', 'STM32F407VGT6' */])
